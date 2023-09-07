4fbea095431ba8ce7f5553313c446a1d5d5e6087
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-09-07 14:20:55 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX  = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/
+
+  with_options presence: true do
+    validates :nickname
+    validates :date_of_birth
+  end
+  
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
