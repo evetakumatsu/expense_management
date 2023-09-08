27d110d3c2b2a71e38d19bf38354abe85773c042
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-09-08 12:05:00 +0900

    deviseカスタムコントローラー設定とdestroyアクションのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
   root "spendings#top"
+  resources :spendings, only: [:index, :destroy]
 end
