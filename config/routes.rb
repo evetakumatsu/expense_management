26fe3018dbf3fc2b06dbc69cb8e766daefec0a30
evetakumatsu <rstoyo.takuya.matsumoto@gmail.com>
2023-09-25 16:30:46 +0900

    年間家計簿のページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
   root "spendings#top"
   resources :spendings, only: [:index, :destroy]
+  resources :year_spendings, only: [:index]
 end
